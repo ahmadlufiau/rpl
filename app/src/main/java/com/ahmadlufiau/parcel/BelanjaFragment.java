628bb5d1a32a9fd0ccca1a38ab5f247d0b6492ce
ahmadlufiau <ahmadlufi300@gmail.com>
2018-01-10 15:47:22 +0700

    Hapus Detail

--- app/src/main/java/com/ahmadlufiau/parcel/BelanjaFragment.java
+++ app/src/main/java/com/ahmadlufiau/parcel/BelanjaFragment.java
@@ -14,7 +14,7 @@ import android.widget.Button;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BelanjaFragment extends Fragment implements View.OnClickListener {
+public class BelanjaFragment extends Fragment {
 
 
     public BelanjaFragment() {
@@ -26,21 +26,8 @@ public class BelanjaFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_belanja, container, false);
-        Button btnDetail = (Button)view.findViewById(R.id.btn_detail);
-        btnDetail.setOnClickListener(this);
-        return view;
+        return inflater.inflate(R.layout.fragment_belanja, container, false);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_detail){
-            DetailParcelFragment mCategoryFragment = new DetailParcelFragment();
-            FragmentManager mFragmentManager = getFragmentManager();
-            FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
-            mFragmentTransaction.replace(R.id.frame_container, mCategoryFragment, DetailParcelFragment.class.getSimpleName());
-            mFragmentTransaction.addToBackStack(null);
-            mFragmentTransaction.commit();
-        }
-    }
+
 }
